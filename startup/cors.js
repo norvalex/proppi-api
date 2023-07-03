95a046ecc5ee44dc66e5f5ded640f61ca30034d2
Alex <norvalex@users.noreply.github.com>
2023-07-03 15:01:11 +0200

    cors fix 2

--- startup/cors.js
+++ startup/cors.js
@@ -1,4 +1,4 @@
-module.exports = app.use(function(req, res, next) {
+module.exports = (function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
